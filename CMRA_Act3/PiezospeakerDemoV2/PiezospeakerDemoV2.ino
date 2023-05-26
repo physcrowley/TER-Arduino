ebe02c79eb8b33a6723ff588d6909f2d507aa72a
physcrowley <53307335+physcrowley@users.noreply.github.com>
2023-05-26 14:00:36 -0400

    update for Python comparison

--- CMRA_Act3/PiezospeakerDemoV2/PiezospeakerDemoV2.ino
+++ CMRA_Act3/PiezospeakerDemoV2/PiezospeakerDemoV2.ino
@@ -1,53 +1,56 @@
-// Piezospeaker Demo
-// Use the piezo speaker to play a tone at one frequency during the setup() function, and
-// then separate frequencies after each forward and turn robot behavior in the loop() function.
+// Ajout de code externe
+#include <Servo.h>
 
-/* 
- *  Remplace les #define avec des définitions de variables
- *  Modifications par : M. Crowley, 2023-03
+// Déclaration des variables globales
+Servo servoLeft;        // moteur gauche
+Servo servoRight;       // moteur droit
+#define speakerPort  4  // prise où le haut-parleur est connecté
+
+/*
+ * Fonctions obligatoires, appelées automatiquement par Arduino
  */
 
-// Variables for storing various servomotor speeds
-int stopServo = 1500;
-int forward = 1700;
-int reverse = 1300;
-
-// Variable for storing speaker port
-int speakerPort = 4;
-
-//Servo Include Library and right and left servo definitions
-#include <Servo.h>
-Servo servoLeft;
-Servo servoRight;
-
-void setup()                                 // Built-in setup() initialization function runs once
+// Appelée une seule fois lors du lancement du programme
+void setup() 
 {
-  servoLeft.attach(10);                      // Set left servo to pin 10
-  servoRight.attach(11);                     // Set right servo to pin 11
+  servoLeft.attach(10);  // lier le moteur gauche à la prise 10
+  servoRight.attach(11); // lier le moteur gauche à la prise 11
 
-  tone(speakerPort, 2000, 1000);             // Play tone for 1 second
-  delay(1000);                               // Delay for tone to finish
+  tone(speakerPort, 2000, 1000); // jouer un ton de 2kHz pendant 1s
+  delay(1000);                   // bloquer le programme pendant 1s
 }
 
-void loop()                                  // Built-in loop repeats forever after setup() function finishes
+// Appelée dans une boucle infinie après la fonction setup()
+void loop() 
 {
-    // Move Forward for 1 second
-    servoLeft.writeMicroseconds(forward);
-    servoRight.writeMicroseconds(reverse);
-    delay(1000);
-    servoLeft.writeMicroseconds(stopServo);
-    servoRight.writeMicroseconds(stopServo);
+  forward(1000); // bouger vers l'avant pendant 1s
+  tone(speakerPort, 500, 1000); // jouer un ton de 500Hz pendant 1s
+  delay(1000);
 
-    tone(speakerPort, 500, 1000);             // Play tone for 1 second
-    delay(1000);  
+  turnRight(400); // tourner vers la droite pendant 0,4s
+  tone(speakerPort, 1500, 1000); // jouer un ton de 1,5kHz pendant 1s
+  delay(1000);
+}
 
-    // Turn Right for 0.4 seconds
-    servoLeft.writeMicroseconds(forward);
-    servoRight.writeMicroseconds(forward);
-    delay(550);
-    servoLeft.writeMicroseconds(stopServo);
-    servoRight.writeMicroseconds(stopServo);
+/*
+ * Fonctions que nous avons définies nous-mêmes
+ */
 
-    tone(speakerPort, 1500, 1000);             // Play tone for 1 second
-    delay(1000);  
+// instructions pour faire avancer le robot pendant un temps spécifique 
+void forward(int time)
+{
+  servoLeft.writeMicroseconds(1700);
+  servoRight.writeMicroseconds(1300);
+  delay(time);
+  servoLeft.writeMicroseconds(1500);
+  servoRight.writeMicroseconds(1500);
+}
+
+// instructions pour faire pivoter le robot vers la droite pendant un temps spécifique
+void turnRight(int time){
+  servoLeft.writeMicroseconds(1700);
+  servoRight.writeMicroseconds(1700);
+  delay(time);
+  servoLeft.writeMicroseconds(1500);
+  servoRight.writeMicroseconds(1500);
 }
